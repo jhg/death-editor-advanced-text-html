d66b0edbe5b66e514ec412cefc1e21e416fcf6df
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2014-01-14 12:16:50 +0000

    Optimization in render of control and add default controls for image and link.

--- jquery.death.js
+++ jquery.death.js
@@ -1,22 +1,28 @@
 (function($){
 
 $.fn.editor = function(options) {
+  // Get settings give or by default
   var settings = $.extend({}, $.fn.editor.defaults, options);
+  // Hide textarea and make editor
   this.addClass(settings.class_editor).after(settings.tag).css('display','none')
     .keyup(refreshEditionArea).each(refreshEditionArea)
     .next().attr('contenteditable', true).addClass(settings.class_editor)
     .focusout(refreshTextArea).keyup(refreshTextArea).change(refreshTextArea);
   // If controls is true then make controls layer
   if (settings.controls != false) {
+    // Make controls container and configure controls
     this.before(settings.tag).prev().addClass(settings.class_controls)
       .each(function(){
         for (i in settings.controls){
+          // Get settings of control
           var control = $.extend({
               render: $.fn.editor.renderControl,
-              config: $.fn.editor.configControl
+              config: $.fn.editor.configControl,
+              action: $.fn.editor.configAction
           }, settings.controls[i]);
+          // Render and configure control
           control.config(
-            $(this).append(control.render(control))
+            $(this).append(control.render())
             .children(':last-child')
           );
         }
@@ -37,15 +43,21 @@ function refreshEditionArea(){
 };
 
 // Render by default
-$.fn.editor.renderControl = function(control){
-    return '<input class="'+control.command+'" type="button" value="'+control.text+'" />';
+$.fn.editor.renderControl = function(){
+    return '<input class="'+this.command+'" type="button" value="'+this.text+'" />';
 };
 
 // Configurator by default
 $.fn.editor.configControl = function(control){
+    var settings = this;
     control.click(function(){
-      document.execCommand(control.attr('class'), null, null);
+      settings.action();
     });
+};
+
+// Action by default
+$.fn.editor.configAction = function(){
+    document.execCommand(this.command, null, null);
 };
 
 // Configuration by default
@@ -53,6 +65,8 @@ $.fn.editor.defaults = {
     controls: [{text:'B',command:'bold'},
                {text:'I',command:'italic'},
                {text:'U',command:'underline'},
+               {text:'img',command:'insertImage',action:function(){document.execCommand(this.command, null, prompt('URL:'));}},
+               {text:'enl',command:'createLink',action:function(){document.execCommand(this.command, null, prompt('URL:'));}},
                {text:'</>',command:'htmlEditor',config:function(control){
                    control.click(function(){
                      var textarea = $(this).parent().next();
